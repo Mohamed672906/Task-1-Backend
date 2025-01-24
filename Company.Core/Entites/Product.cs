4c0e752867aac196b356dd8fa896ea4f2f2a16eb
Mohamed Ali <mohamedalibk66@gmail.com>
2025-01-24 17:41:41 +0200

    Product Or Order

---
+++ Company.Core/Entites/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Core.Entites
+{
+    public class Product
+    {
+
+        public int ProductId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public Decimal Price { get; set; }
+
+        public int Stock { get; set; }
+
+
+        public int OrderId { get; set; } //Fk
+        public Order order { get; set;  }
+
+    }
+}
